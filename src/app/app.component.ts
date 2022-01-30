c8d44d7331c30d0ba5ac855b414159f20922a968
Obaid Nasary <obaidnasary@users.noreply.github.com>
2022-01-30 13:43:02 +0200

    Adding print valid and invalid log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { i18nMetaToJSDoc } from '@angular/compiler/src/render3/view/i18n/meta';
+import { invalid } from '@angular/compiler/src/render3/view/util';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+// import { resourceLimits } from 'worker_threads';
 import { Iban } from './iban';
 import { IbanService } from './iban.service';
 
@@ -13,7 +15,10 @@ import { IbanService } from './iban.service';
 })
 export class AppComponent implements OnInit{
   public ibans!: Iban[];
-  public checkIban!: Iban;
+  // public checkIban!: Iban;
+  // public ibana!: Iban [];
+  public validIbans!: Iban[];
+  public changeModelTitle!: string;
 
 
   constructor(private ibanService: IbanService){}
@@ -33,8 +38,32 @@ export class AppComponent implements OnInit{
     );
   }
 
+  public getValidIbans(): void{
+    this.ibanService.getIbanByValid(1).subscribe(
+      (response: Iban[]) =>{
+        this.validIbans = response;
+        console.log(response);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public getInValidIbans(): void{
+    this.ibanService.getIbanByValid(0).subscribe(
+      (response: Iban[]) =>{
+        this.validIbans = response;
+        console.log(response);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
   public onAddIban(addForm: NgForm): void{
-    document.getElementById('add-employee-form')!.click();
+    document.getElementById('add-iban-form')!.click();
       this.ibanService.addIban(addForm.value).subscribe(
         (response: Iban) => {
           console.log(response);
@@ -46,7 +75,8 @@ export class AppComponent implements OnInit{
       );
   }
 
-  public onOpenModal(iban: Iban, mode: string): void{
+  public onOpenModal(mode: string): void{
+    console.log('hi');
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
     button.type = 'button';
@@ -56,10 +86,15 @@ export class AppComponent implements OnInit{
       button.setAttribute('data-target', '#addIbanModal');
     }
     if (mode === 'theValid'){
+      console.log('hello');
       button.setAttribute('data-target', '#printValidModal');
+      this.getValidIbans();
+      this.changeModelTitle = "Valid IBAN Numbers Log";
     }
     if (mode === 'theInvalid'){
-      button.setAttribute('data-target', '#printInValidModal');
+      button.setAttribute('data-target', '#printValidModal');
+      this.getInValidIbans();
+      this.changeModelTitle = "Invalid IBAN Numbers Log";
     }
 
     container?.appendChild(button);
